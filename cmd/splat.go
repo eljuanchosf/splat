cde99802155ca139c0e17b43f9cc7c94297d1cb8
Juan Pablo Genovese <juanpgenovese@gmail.com>
2017-04-20 18:58:29 -0300

    Remove boilerplate code

--- cmd/splat.go
+++ cmd/splat.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
-
 func main() {
-
-	fileName := "/home/juan/dev/go/splat/fixtures/good-yaml.yml"
-	yamlFile, _ := ParseFile(fileName)
-	fmt.Println(reflect.TypeOf(yamlFile.Lines))
-	// for _, line := range yamlFile.Lines {
-	// 	fmt.Println(line.Key)
-	// }
 }
